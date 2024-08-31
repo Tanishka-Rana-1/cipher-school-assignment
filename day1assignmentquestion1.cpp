b59aee9e67cd0ea5919bc14411f09ca6a8090387
Tanishka-Rana-1 <152804156+Tanishka-Rana-1@users.noreply.github.com>
2024-08-31 21:49:38 +0530

    Update day1assignmentquestion1.cpp

--- day1assignmentquestion1.cpp
+++ day1assignmentquestion1.cpp
@@ -3,20 +3,11 @@
 
 int main() {
    string a, b, c;
-
-   
     std::cout << "Enter the first string: ";
     std::getline(std::cin, a);
-
-
     std::cout << "Enter the second string: ";
     std::getline(std::cin, b);
-
-
     c = a + b;
-
-
     std::cout << "Concatenated string: " << c<< std::endl;
-
-    return 0;
+ return 0;
 }
